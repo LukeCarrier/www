51e3b82d0d1d08972e36e022e7dca4c96dd593fc
Luke Carrier <luke@carrier.family>
2026-02-20 22:17:56 +0000

    flake

---
+++ nix/flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "LukeCarrier/www";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-25.11";
+  };
+
+  outputs =
+    { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [ pkgs.zola ];
+        };
+      }
+    );
+}
